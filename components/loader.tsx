2fc781fa76deddd7daca8c218740d3da8d9c6df7
Simone Bartoli <s.bartoli@se20.qmul.ac.uk>
2022-05-31 12:25:21 +0100

    Refactor Problem Fixed 2.0

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from "../../ivaldi-italian-food/styles/loader.module.css"
+import styles from "../styles/loader.module.css"
 
 const Loader = () => {
     return (
