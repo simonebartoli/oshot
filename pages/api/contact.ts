aef96f9a698f12abf5960e2beecd7bd2d0bd662a
Simone Bartoli <s.bartoli@se20.qmul.ac.uk>
2022-06-11 15:07:08 +0100

    Mail Updated

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -21,12 +21,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         }
 
         const clientEmailSource = fs.readFileSync(path.join(process.cwd(), "/data/email/contact.hbs"))
-        const templateClient = handlebars.compile(clientEmailSource.toString())
-        const htmlToSendClient = templateClient({name: name, surname: surname, email: email, phone: phone, message: message})
-
-        const adminEmailSource = fs.readFileSync(path.join(process.cwd(), "/data/email/reference.hbs"))
-        const templateAdmin = handlebars.compile(adminEmailSource.toString())
-        const htmlToSendAdmin = templateAdmin({name: name, surname: surname, email: email, phone: phone, message: message})
+        const template = handlebars.compile(clientEmailSource.toString())
+        const htmlToSendClient = template({
+            fullname: `${name} ${surname}`,
+            header: "Your Request Has Been Received",
+            text1: "We want to inform you that you request has been received correctly.",
+            text2: "We aim to respond you within the next 48 hours.",
+            text3: "You can find the details of your request below.",
+            email: email,
+            phone: phone,
+            message: message
+        })
+        const htmlToSendAdmin = template({
+            fullname: `Administrator`,
+            header: "A New Client Request Has Been Received",
+            text1: "Oshot UK system wants to inform you that a new client request request has been received correctly.",
+            text2: "Consider to respond within the next 48 hours.",
+            text3: "You can find the details of the request below.",
+            email: email,
+            phone: phone,
+            message: message
+        })
 
         await sendEmail(name, surname, email, phone, message, htmlToSendClient, htmlToSendAdmin)
         res.status(200).json("OK")
@@ -73,7 +88,7 @@ const sendEmail = async (name: string, surname: string, email: string, phone: st
         subject: "Dr SW Clinic - Contact Request Received", // Subject line
         attachments: [{
             filename: 'logo.jpg',
-            path: process.cwd() +'/data/email/logo.jpg',
+            path: process.cwd() +'/data/email/logo.png',
             cid: 'logo'
         }],
         html: htmlToSendClient, // html body
@@ -85,7 +100,7 @@ const sendEmail = async (name: string, surname: string, email: string, phone: st
         subject: "Dr SW Clinic - A new request received",
         attachments: [{
             filename: 'logo.jpg',
-            path: process.cwd() +'/data/email/logo.jpg',
+            path: process.cwd() +'/data/email/logo.png',
             cid: 'logo'
         }],
         html: htmlToSendAdmin, // html body
